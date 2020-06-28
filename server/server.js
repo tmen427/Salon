e17a58f7e5057247705ee8330891b5a2642b9333
tmen427 <menx0003@umn.edu>
2020-06-27 23:41:31 -0500

    updates build

--- server/server.js
+++ server/server.js
@@ -45,10 +45,10 @@ app.get("/api/hello", (req, res) => {
 //}
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "../build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
   });
 }
 
