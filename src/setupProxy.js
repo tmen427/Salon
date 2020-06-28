ab23702ad1ee1242f1bdcaa2ca1aa6008f3ad8b6
tmen427 <menx0003@umn.edu>
2020-06-27 21:15:19 -0500

    updates

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = app => {
-  app.use(createProxyMiddleware("/api/*", { target: "http://localhost:3306/" }));
+  app.use(createProxyMiddleware("/api/*", { target: "http://localhost:5000/" }));
 };
