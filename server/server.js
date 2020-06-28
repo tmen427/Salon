9bcfe8694107b73e30b50c8364987eabc4ed4bce
tmen427 <menx0003@umn.edu>
2020-06-27 19:18:43 -0500

    change port

--- server/server.js
+++ server/server.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === "production") {
 
 //const port = process.env.PORT || 5000;
 //app.listen(port);
-app.listen(process.env.PORT || 5000, function(){
+app.listen(process.env.PORT || 3306, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
