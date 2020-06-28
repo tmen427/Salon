c5254c8decfa9ce2204476e3593fa6dd623113d8
tmen427 <menx0003@umn.edu>
2020-06-27 23:25:28 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ console.log("Alright i think the post worked!")
  })
    .then(function(response) {
      if (response.status >= 400) {
-       console.log(response);
+     
        throw new Error("Bad response from server");
      }
      return response.json();
