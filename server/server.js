b266b8a70e29d0838250de37d9c5b02b7acb8562
tmen427 <menx0003@umn.edu>
2020-07-15 18:30:53 -0500

    updates

--- server/server.js
+++ server/server.js
@@ -54,13 +54,13 @@ app.get("/api/hello", (req, res) => {
   
   // Handles any requests that don't match the ones above
   //u need to include the below code this so that the back knows where to route and (connect with react on the front end!!!)
-//app.get("*", (req, res) => {
-  //  res.sendFile(path.join(__dirname, "../public/index.html"));
- // });
-  // Express only serves static assets in production
-//if (process.env.NODE_ENV === "production") {
-//  app.use(express.static("src/index.js"));
-//}
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+//   Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("src/index.js"));
+}
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static(path.join(__dirname, "../build")));
