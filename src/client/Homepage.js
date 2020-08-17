43909b958416f565dc7b3e742bb0fa553b11283f
tmen427 <menx0003@umn.edu>
2020-08-17 10:37:14 -0500

    updaes

--- src/client/Homepage.js
+++ src/client/Homepage.js
@@ -19,27 +19,12 @@ return (
 
 
 
-<div style={{background:"purple", width:"100%", height: "500px", padding: "100px"}}>
+<div className="container-fluid" style={{background:"purple", width:"100%", height: "500px", padding: "100px"}}>
 <div class="row">
     <div class="col">
 
-  
-   <h1>Come visit us! </h1>
-   <p style={{fontSize: "20px", overflow: "auto"}}>
- <h3>Store Hours: </h3> 
-Monday	10:00 am - 7:00 pm<br></br>
-Tuesday	10:00 am - 7:00 pm<br></br>
+    </div>
 
-Wednesday	10:00 am - 7:00 pm<br></br>
-Thursday	10:00 am - 7:00 pm<br></br>
-Friday	9:30 am - 7:30 pm<br></br>
-Saturday	9:30 am - 6:30 pm<br></br>
-Sunday	Closed
-  </p>
-    </div>
-    <div class="col">
-<img src={inside} className="img-fluid" style={{marginLeft: "80px", maxWidth: "500px", maxHeight: "500px"}} ></img>
-    </div>
   </div>
 
 </div>
@@ -48,29 +33,35 @@ Sunday	Closed
 
 
 <div>
-<h1 style={{textAlign: "center", padding: "60px", fontSize:"50px" }}>Our Services</h1>
-<div class="container" style={{padding: "40px 40px 100px 40px"}}>
+<h1 style={{textAlign: "center", padding: "60px", fontSize:"50px",fontWeight:"700" }}>Our Services</h1>
+<div  style={{padding: "40px 40px 100px 40px"}}>
   <div class="row">
     <div class="col-sm">
 
-      <img src={manicure} className="img-fluid " alt="Responsive image"></img>
+      <img src={manicure} className="img-fluid " alt="Responsive image" style={{minHeight:"150px"}}></img>
       <h1 style={{background: "purple", textAlign: "center", color:"white"}}>Pedicure</h1>
     </div>
     <div class="col-xl">
-    <img src={pedicure} class="img-fluid" alt="Responsive image"></img>
+    <img src={pedicure} class="img-fluid" alt="Responsive image" style={{minHeight:"150px"}}></img>
     <h1 style={{background: "purple", textAlign: "center", color:"white"}}>Manicure</h1>
     </div>
     <div class="col-xl">
-    <img src={hands} class="img-fluid" alt="Responsive image"></img>
-    <h1 style={{background: "purple", textAlign: "center", color:"white"}}>Waxing</h1>
+    <img src={hands} class="img-fluid" alt="Responsive image" style={{minHeight:"150px"}}></img>
+    <h1 style={{background: "purple", textAlign: "center", color:"white"}}>Wax</h1>
   
     </div>
+    <div class="col-sm">
+
+<img src={manicure} className="img-fluid " alt="Responsive image" style={{minHeight:"150px", minWidth:"150px"}}></img>
+<h1 style={{background: "purple", textAlign: "center", color:"white"}}>Special</h1>
+</div>
+
   </div>
 </div>
 </div>
 
 <footer class="footer">
-<div class="container">
+<div >
 <div class="row">
     <div class="col">
 
