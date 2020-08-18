2c5233358a52eb9d2989526ec645be0893dd7534
tmen427 <menx0003@umn.edu>
2020-08-18 12:29:31 -0500

    updates

--- src/client/NavBar.js
+++ src/client/NavBar.js
@@ -40,14 +40,15 @@ function NavBar () {
 
 return (
 
-  <div >
-  <div id="topbar" class="d-flex align-items-center fixed-top">
+<div>
+ 
+<div id="topbar" class="d-flex align-items-center fixed-top">
     <div class="container d-flex">
       <div class="contact-info mr-auto">
-        <i class="icofont-phone"></i> +1 5589 55488 55   {users}
-        <span class="d-none d-lg-inline-block"><i class="icofont-clock-time icofont-rotate-180"></i> Mon-Sat: 11:00 AM - 23:00 PM</span>
+        <i class="icofont-phone"></i> +1 5589 55488 55
+        <span class="contact-info mr-auto"><i class="icofont-clock-time icofont-rotate-180"></i> Mon-Sat: 11:00 AM - 23:00 PM</span>
       </div>
-  
+   
     </div>
   </div>
 
@@ -55,20 +56,15 @@ return (
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="index.html">La Belle Nail Salon and Spa</a></h1>
+      <h1 class="logo mr-auto"><a href="index.html">La Belle Salon and Spa</a></h1>
 
-      <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></img></a>
 
-      <nav class="nav-menu d-none d-lg-block">
+      <nav class="nav-menu d-sm d-lg-block">
         <ul>
-          <li class="active"><a href="/">Home</a></li>
+          <li class="active"><a href="index.html">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#menu">Menu</a></li>
-          <li><a href="#specials">Specials</a></li>
-          <li><a href="#events">Events</a></li>
-          <li><a href="#gallery">Gallery</a></li>
-          <li><a href="#chefs">Chefs</a></li>
-          <li><a href="#contact">Contact</a></li>
+    
           <li class="book-a-table text-center"><a href="#book-a-table">Book a table</a></li>
         </ul>
       </nav>
@@ -76,10 +72,8 @@ return (
     </div>
   </header>
 
-
-
-
   </div>
+
 
 )
 }
