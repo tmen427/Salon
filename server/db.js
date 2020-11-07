4ada03c6e19f6cf68c12668468ef3d83b7240a72
tmen427 <menx0003@umn.edu>
2020-11-07 11:28:47 -0600

    updats

--- server/db.js
+++ server/db.js
@@ -5,15 +5,21 @@ var connection;
 //if deploying on heroku
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL); }
-  else {
+  //if deploying to AWS
+  else if ({
     connection = mysql.createConnection({
-       host:	'localhost',
-       port:	'8889',
-        user:	'root',
-       password:	'root', 
+       host:	'database-1.ck5ruutdz55a.us-east-2.rds.amazonaws.com',
+       port:	'3306',
+        user:	'admin',
+       password:	'Mortalkombat10!', 
       database: 'Salon'
+  })
   });
-  };
+ 
+
+
+
+
 
 //make the connection to the database
 
